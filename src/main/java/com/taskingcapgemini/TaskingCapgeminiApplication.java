603d52e48ea0563978e5552bf0ea4ed08d2fdc10
andres arcila <andres.arcila@konexinnovation.com.co>
2024-08-09 22:40:43 -0500

    Se implementa la arquitectura inicial del proyecto

---
+++ src/main/java/com/taskingcapgemini/TaskingCapgeminiApplication.java
@@ -0,0 +1,13 @@
+package com.taskingcapgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskingCapgeminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskingCapgeminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasking_capgemini/TaskingCapgeminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasking_capgemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskingCapgeminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
